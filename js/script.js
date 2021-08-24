eb798998b8b5b5f8113284125d4b07d522717823
efernandev <contato.dudu087@gmail.com>
2021-08-23 22:21:59 -0300

    folder with file javascript updated

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
+
 function insert(num) {
     document.form.textview.value = document.form.textview.value+num;
 }
 
-function equal() {
+function result() {
     var exp = document.form.textview.value;
     if (exp) {
         document.form.textview.value = eval(exp);
     }
 }
 
-function Clean() {
+function reset() {
     document.form.textview.value = "";
 }
 
-function back() {
+function removeLetter() {
     var exp = document.form.textview.value;
     document.form.textview.value = exp.substring(0, exp.length-1)
 }
